47c9a737e11fb2c9dee420ad6c993ece02546f5c
Vasjan Çupri <vasjancupri04@gmail.com>
2023-08-20 17:41:24 +0300

    Added the extra weather info section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,10 @@
 import './Main.css'
 import CurrentWeather from '../CurrentWeather/CurrentWeather'
+import WeatherExtra from '../WeatherExtra/WeatherExtra'
 
 export default function Main() {
   const mainContainer = document.createElement('main')
-  mainContainer.append(
+  mainContainer.appendChild(
     CurrentWeather({
       max: 34,
       min: 22,
@@ -12,6 +13,16 @@ export default function Main() {
       country: 'Albania',
       sunrise: '05:54 AM',
       sunset: '19:35 PM',
+    })
+  )
+  mainContainer.appendChild(
+    WeatherExtra({
+      wind: '1.5m/s N',
+      pressure: '1012hPA',
+      humidity: '52%',
+      uv: 8,
+      visibility: '10.0km',
+      dewPoint: '22°C',
     })
   )
 
---
+++ src/components/WeatherExtra/WeatherExtra.js
@@ -0,0 +1,39 @@
+import './WeatherExtra.css'
+export default function WeatherExtra(extra) {
+  const weatherExtraContainer = document.createElement('div')
+  weatherExtraContainer.setAttribute('id', 'extra-weather')
+
+  const weatherExtraFirstColumn = document.createElement('div')
+  const weatherExtraSecondColumn = document.createElement('div')
+  const windText = document.createElement('p')
+  windText.innerHTML = `wind: <span>${extra.wind}</span>`
+  const pressureText = document.createElement('p')
+  pressureText.innerHTML = `pressure: <span>${extra.pressure}</span>`
+  const humidityText = document.createElement('p')
+  humidityText.innerHTML = `humidity: <span>${extra.humidity}</span>`
+  const uvText = document.createElement('p')
+  uvText.innerHTML = `uv: <span>${extra.uv}</span>`
+  const visibilityText = document.createElement('p')
+  visibilityText.innerHTML = `visibility: <span>${extra.visibility}</span>`
+  const dewPointText = document.createElement('p')
+  dewPointText.innerHTML = `dew point: <span>${extra.dewPointText}</span>`
+
+  weatherExtraFirstColumn.appendChild(windText)
+  weatherExtraFirstColumn.appendChild(pressureText)
+  weatherExtraFirstColumn.appendChild(humidityText)
+  weatherExtraSecondColumn.appendChild(uvText)
+  weatherExtraSecondColumn.appendChild(visibilityText)
+  weatherExtraSecondColumn.appendChild(dewPointText)
+
+  weatherExtraContainer.appendChild(weatherExtraFirstColumn)
+  weatherExtraContainer.appendChild(weatherExtraSecondColumn)
+
+  return weatherExtraContainer
+}
+
+// wind: '1.5m/s N',
+// pressure: '1012hPA',
+// humidity: '52%',
+// uv: 8,
+// visibility: '10.0km',
+// dewPoint: '22°C',
